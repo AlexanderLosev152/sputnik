838361f9945fe2dafaf09a2d95648d359bcf12d2
Alexander <losev.pavlovo@mail.ru>
2024-03-25 17:31:38 +0300

    gallery

--- js/main.js
+++ js/main.js
@@ -7,4 +7,12 @@ $(function () {
     slidesToShow: 1,
     adaptiveHeight: true,
   });
+
+  $(".spoiler__box-title").click(function (event) {
+    if ($(".spoiler__wrapper").hasClass("one")) {
+      $(".spoiler__box-title").not($(this)).removeClass("active");
+      $(".spoiler__box-text").not($(this).next()).slideUp("300");
+    }
+    $(this).toggleClass("active").next().slideToggle(300);
+  });
 });
