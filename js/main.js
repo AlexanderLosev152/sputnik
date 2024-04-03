dadee7acb18e4ac2669528613ed86e86832965c6
losev__152 <93211887+AlexanderLosev152@users.noreply.github.com>
2024-04-03 13:22:57 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 $(function () {
-  $(".slider-wrapper").slick({
+  $('.slider-wrapper').slick({
     dots: true,
     arrows: false,
     infinite: true,
     speed: 300,
     slidesToShow: 1,
     adaptiveHeight: true,
-  });
+  })
 
-  $(".spoiler__box-title").click(function (event) {
-    if ($(".spoiler__wrapper").hasClass("one")) {
-      $(".spoiler__box-title").not($(this)).removeClass("active");
-      $(".spoiler__box-text").not($(this).next()).slideUp("300");
+  $('.spoiler__box-title').click(function (event) {
+    if ($('.spoiler__wrapper').hasClass('one')) {
+      $('.spoiler__box-title').not($(this)).removeClass('active')
+      $('.spoiler__box-text').not($(this).next()).slideUp('300')
     }
-    $(this).toggleClass("active").next().slideToggle(300);
-  });
-});
+    $(this).toggleClass('active').next().slideToggle(300)
+  })
+})
